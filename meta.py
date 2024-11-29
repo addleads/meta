c7f776e9926b6637db0a020297966de040d0c147
addleads <addleadstation@gmail.com>
2024-11-29 16:25:42 -0300

    Update meta.py

--- meta.py
+++ meta.py
@@ -10,5 +10,18 @@ st_autorefresh(interval=5 * 1000, key="auto_refresh")
 # URL do iframe que você deseja exibir
 iframe_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRz00dW2oP24--Se7nDtOs2NyOkcY-5Pi70JB36UmA885elN5_jGR9tzOeSxW6hD7Q18QRamuyKjm87/pubchart?oid=1606736902&format=interactive"
 
-# Exibe o iframe na aplicação Streamlit
-st.markdown(f'<iframe src="{iframe_url}" width="100%" height="600" frameborder="0"></iframe>', unsafe_allow_html=True)
+# Exibe o iframe na aplicação Streamlit com zoom de 80%
+st.markdown(f'''
+    <style>
+        .iframe-container {{
+            transform: scale(0.8); /* Reduz o tamanho para 80% */
+            transform-origin: top left; /* Define a origem da transformação */
+            width: 125%; /* Ajusta a largura para compensar o zoom */
+            height: 125%; /* Ajusta a altura para compensar o zoom */
+            overflow: hidden; /* Oculta qualquer conteúdo que exceda o contêiner */
+        }}
+    </style>
+    <div class="iframe-container">
+        <iframe src="{iframe_url}" width="100%" height="600" frameborder="0"></iframe>
+    </div>
+''', unsafe_allow_html=True)
