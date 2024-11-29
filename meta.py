974480982d3e1f39b3388b4369913b34aa6f4ecf
addleads <addleadstation@gmail.com>
2024-11-29 16:31:41 -0300

    Update meta.py

--- meta.py
+++ meta.py
@@ -10,18 +10,19 @@ st_autorefresh(interval=5 * 1000, key="auto_refresh")
 # URL do iframe que você deseja exibir
 iframe_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRz00dW2oP24--Se7nDtOs2NyOkcY-5Pi70JB36UmA885elN5_jGR9tzOeSxW6hD7Q18QRamuyKjm87/pubchart?oid=1606736902&format=interactive"
 
-# Define a escala desejada (por exemplo, 120% para ampliar)
-scale_factor = 1.2
-
-# Define as dimensões originais do iframe
-original_width = 800  # Largura original em pixels
-original_height = 600  # Altura original em pixels
-
-# Calcula as novas dimensões do iframe
-scaled_width = int(original_width * scale_factor)
-scaled_height = int(original_height * scale_factor)
-
-# Exibe o iframe na aplicação Streamlit com as novas dimensões
+# HTML e JavaScript para aumentar o zoom do iframe
 st.markdown(f'''
-    <iframe src="{iframe_url}" width="{scaled_width}" height="{scaled_height}" frameborder="0"></iframe>
+    <div style="position: relative; width: 100%; height: 600px; overflow: hidden;">
+        <iframe id="myIframe" src="{iframe_url}" width="100%" height="100%" frameborder="0"></iframe>
+    </div>
+    <button onclick="zoomIn()">Aumentar Zoom</button>
+    <script>
+        function zoomIn() {{
+            var iframe = document.getElementById('myIframe');
+            for (var i = 0; i < 5; i++) {{
+                iframe.style.transform = 'scale(' + (1 + (i * 0.1)) + ')';
+                iframe.style.transformOrigin = 'top left';
+            }}
+        }}
+    </script>
 ''', unsafe_allow_html=True)
